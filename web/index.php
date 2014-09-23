1a45b5d824e3b584418e8e343cea91b878dca425
guosi <cc.guo@musikid.com>
2014-09-23 22:10:16 +0800

    屏蔽 $_SERVER['argv'] 未定义的警告

--- web/index.php
+++ web/index.php
@@ -1,6 +1,10 @@
 <?php 
+// 屏蔽 $_SERVER['argv'] 未定义的警告
+!isset($_SERVER['argv']) && $_SERVER['argv'] = array();
+
 // 活动目录切换标示
 define('STDIN', 1);
+
 // 引入框架原生 index.php
 include '../codeigniter/2.2.0/index.php';
 ?>
