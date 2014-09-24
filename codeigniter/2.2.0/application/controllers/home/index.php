8127f71aa39d59cff5866f553161293e1d1712db
guosi <cc.guo@musikid.com>
2014-09-24 11:06:36 +0800

    前后台分组

---
+++ codeigniter/2.2.0/application/controllers/home/index.php
@@ -0,0 +1,31 @@
+<?php 
+
+class Index extends CI_Controller{
+	/**
+	* 构造函数
+	* @author guosi <cc.guo@musikid.com>
+	* @return void
+	* @version 2014-9-24
+	**/
+	
+	function __construct()
+	{
+	   parent::__construct();
+	}
+	// --------------------------------------------------------------------
+	
+	/**
+	* 网站首页
+	* @author guosi <cc.guo@musikid.com>
+	* @return void
+	* @version 2014-9-24
+	**/
+	
+	function index()
+	{
+	   die('j');
+	}
+	// --------------------------------------------------------------------
+}
+
+?>
